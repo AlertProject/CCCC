79d8c3fc23eff1c010cdde578d52bb6bef569099
Niz <daisuke_niizumi@balmuda.com>
2016-03-18 20:28:59 +0900

    Fixed build issue
    As reported at https://sourceforge.net/p/cccc/bugs/55/

--- cccc/cccc_tbl.cc
+++ cccc/cccc_tbl.cc
@@ -96,7 +96,7 @@ bool CCCC_Table<T>::remove(T* old_item_ptr)
   typename map_t::iterator value_iterator=map_t::find(old_item_ptr->key());
   if(value_iterator!=map_t::end())
     {
-      erase(value_iterator);
+      map_t::erase(value_iterator);
       retval=true;
     }
   return retval;
